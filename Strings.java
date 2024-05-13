d4b24bee1b2b27b1ecbc6a42374fc455a4caf291
Martin Mathurine <42855193+martinmathurine@users.noreply.github.com>
2024-05-13 03:01:21 +0100

    Strings

---
+++ Strings.java
@@ -0,0 +1,26 @@
+class Demo4
+{
+   public static void main(String[] args)
+   	{	
+	   Demo1method();
+	   System.out.println();
+	   Demo3method();
+    
+	}
+//a method to print Hello World   
+public static void Demo1method()
+	{
+           System.out.println("Hello World");
+   
+	}
+
+    //a method to print and and office address
+    public static void Demo3method()
+    	{
+
+    	   System.out.println("Martin Mathurine");
+    	   System.out.println("GitHub University" +"\n" +    
+		"Earth\n"+ "616");
+    
+	}
+}
