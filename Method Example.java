a3301871f5db8024991d4b993fe9abb04762bc29
Martin Mathurine <42855193+martinmathurine@users.noreply.github.com>
2024-05-13 03:02:47 +0100

    Method Example

---
+++ Method Example.java
@@ -0,0 +1,24 @@
+class method_example
+
+{
+	public static void main (String args[])
+	{
+	    firstMethod();// calling first method
+		secondMethod();
+        firstMethod();// calling first method
+		secondMethod();
+	}
+	
+	public static void firstMethod()//creating method
+	{
+		System.out.println("This is first method");
+
+		
+	}
+	
+	
+	public static void secondMethod()
+	{
+		System.out.println("This is second method");
+	}
+}
