052f8e6f7c6bce5b0234bdaea41225fdd0b8f769
Martin Mathurine <42855193+martinmathurine@users.noreply.github.com>
2024-05-13 02:54:35 +0100

    Hi!

---
+++ Hello.java
@@ -0,0 +1,16 @@
+class FirstSimpleProgram
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello Martin");
+      System.out.println();
+      System.out.println();
+      System.out.println("Middlesex University "+"\n"+"The Burroughs\n"+"Hendon NW4 4BT");
+      System.out.println();
+      System.out.println();
+      System.out.println("Hello Martin");
+            /* Here is 
+             my programme
+            */
+   }//end of method
+}
